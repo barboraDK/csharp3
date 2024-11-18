8af1671cdececebbb6bda205bc29da74dca0397b
barboraDK <barbora.drahokoupilova@gmail.com>
2024-11-18 17:44:02 +0100

    0.8_Fronted_Dashboard

--- ToDoList/src/ToDoList.WebApi/Program.cs
+++ ToDoList/src/ToDoList.WebApi/Program.cs
@@ -5,7 +5,11 @@ using ToDoList.Domain.Models;
 var builder = WebApplication.CreateBuilder(args);
 {
     //Configure DI
+    //WebApi Services
     builder.Services.AddControllers();
+    builder.Services.AddSwaggerGen();
+
+    //Persistence services
     builder.Services.AddDbContext<ToDoItemsContext>();
     builder.Services.AddScoped<IRepository<ToDoItem>, ToDoItemsRepository>();
 }
@@ -14,6 +18,8 @@ var app = builder.Build();
 {
     //Configure Middleware (HTTP request pipeline)
     app.MapControllers();
+    app.UseSwagger();
+    app.UseSwaggerUI(config => config.SwaggerEndpoint("/swagger/v1/swagger.json", "ToDoList API V1"));
 }
 
 app.Run();
