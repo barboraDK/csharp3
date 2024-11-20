46c0856f011c86ce7a7e5a2653fe789b89bd4707
barboraDK <barbora.drahokoupilova@gmail.com>
2024-11-20 20:38:28 +0100

    FE_invalidIds

---
+++ ToDoList/src/ToDoList.Frontend/Clients/IToDoItemsClient.cs
@@ -0,0 +1,9 @@
+
+namespace ToDoList.Frontend.Clients;
+
+using ToDoList.Frontend.Views;
+
+public interface IToDoItemsClient
+{
+    public Task<List<ToDoItemView>> ReadItemsAsync();
+}
---
+++ ToDoList/tests/ToDoList.Test/UnitTests/GetTests.cs
@@ -0,0 +1,84 @@
+namespace ToDoList.Test.UnitTests;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using NSubstitute.ReturnsExtensions;
+using ToDoList.Domain.Models;
+using ToDoList.Persistence.Repositories;
+using ToDoList.WebApi.Controllers;
+
+
+public class GetUnitTests
+{
+    [Fact]
+    public void Get_ReadAllAndSomeItemIsAvailable_ReturnsOk()
+    {
+        //Arrange
+        var repositoryMock = Substitute.For<IRepository<ToDoItem>>();
+        var controller = new ToDoItemsController(repositoryMock);
+        /*
+        repositoryMock.When().Do();
+        repositoryMock.Read().Returns();
+        repositoryMock.Read().Throws();
+        repositoryMock.Received().Read();
+        */
+        repositoryMock.Read().Returns(
+            [
+                new ToDoItem {
+                    Name = "testName",
+                    Description = "testDescription",
+                    IsCompleted = false
+                    }
+                ]
+                );
+
+
+        //Act
+        var result = controller.Read();
+        var resultResult = result.Result;
+
+        //Assert
+        Assert.IsType<OkObjectResult>(resultResult);
+        repositoryMock.Received(1).Read();
+    }
+
+    [Fact]
+    public void Get_ReadAllNoItemAvailable_ReturnNotFound()
+    {
+        //Arrange
+        var repositoryMock = Substitute.For<IRepository<ToDoItem>>();
+        var controller = new ToDoItemsController(repositoryMock);
+        repositoryMock.Read().ReturnsNull();
+
+        //Act
+        var result = controller.Read();
+        var resultResult = result.Result;
+
+        //Assert
+        Assert.IsType<NotFoundResult>(resultResult);
+        repositoryMock.Received(1).Read();
+        Assert.Equivalent(new StatusCodeResult(StatusCodes.Status404NotFound), resultResult);
+    }
+
+    [Fact]
+    public void Get_ReadAllExceptionOccured_ReturnInternalServerError()
+    {
+        //Arrange
+        var repositoryMock = Substitute.For<IRepository<ToDoItem>>();
+        var controller = new ToDoItemsController(repositoryMock);
+        repositoryMock.Read().Throws(new Exception());
+
+        //Act
+        var result = controller.Read();
+        var resultResult = result.Result;
+
+        //Assert
+        Assert.IsType<ObjectResult>(resultResult);
+        repositoryMock.Received(1).Read();
+        Assert.Equivalent(new StatusCodeResult(StatusCodes.Status500InternalServerError), resultResult);
+    }
+
+}
+
