c4e52228bda19d0b9735d17e0fd3107720dc96c5
barboraDK <barbora.drahokoupilova@gmail.com>
2024-12-02 19:30:42 +0100

    Assignment 10.1

--- ToDoList/src/ToDoList.Domain/DTOs/ToDoItemGetResponseDto.cs
+++ ToDoList/src/ToDoList.Domain/DTOs/ToDoItemGetResponseDto.cs
@@ -1,8 +1,7 @@
 namespace ToDoList.Domain.DTOs;
 using ToDoList.Domain.Models;
 
-//ma to byt string? Category
-public record class ToDoItemGetResponseDto(int Id, string Name, string Description, bool IsCompleted, string Category) //let client know the Id
+public record class ToDoItemGetResponseDto(int Id, string Name, string Description, bool IsCompleted, string? Category) //let client know the Id
 {
     public static ToDoItemGetResponseDto FromDomain(ToDoItem item) => new(item.ToDoItemId, item.Name, item.Description, item.IsCompleted, item.Category);
 }
--- ToDoList/src/ToDoList.Domain/DTOs/ToDoItemUpdateRequestDto.cs
+++ ToDoList/src/ToDoList.Domain/DTOs/ToDoItemUpdateRequestDto.cs
@@ -1,8 +1,7 @@
 namespace ToDoList.Domain.DTOs;
 using ToDoList.Domain.Models;
 
-//ma to byt string? Category
-public record class ToDoItemUpdateRequestDto(string Name, string Description, bool IsCompleted, string Category)
+public record class ToDoItemUpdateRequestDto(string Name, string Description, bool IsCompleted, string? Category)
 {
     public ToDoItem ToDomain() => new() { Name = Name, Description = Description, IsCompleted = IsCompleted, Category = Category };
 }
